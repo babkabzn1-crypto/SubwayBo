9bac7d79e32b91ee942cea29b21abe8bad334735
babkabzn1-crypto <babkabzn1@gmail.com>
2026-08-19 18:18:57 +0330

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,32 @@
+class SubwayBot:
+
+    def __init__(self):
+        self.lane = 1
+
+    def decide(self, blocked):
+
+        if not blocked[self.lane]:
+            return "NOTHING"
+
+        free = [
+            i for i in range(3)
+            if not blocked[i]
+        ]
+
+        if not free:
+            return "JUMP"
+
+        target = min(
+            free,
+            key=lambda x: abs(x - self.lane)
+        )
+
+        if target < self.lane:
+            self.lane = target
+            return "LEFT"
+
+        if target > self.lane:
+            self.lane = target
+            return "RIGHT"
+
+        return "NOTHING"
